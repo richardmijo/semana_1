5004bb88b8c847816777d5827366ad69ea37c9ec
Richard Armijos <richardmijo@hotmail.com>
2024-05-01 20:22:38 -0500

    creacion de lista

---
+++ src/main/java/ec/gob/loja/semana_1/ListNode.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.gob.loja.semana_1;
+
+/**
+ *
+ * @author desarrollo
+ */
+public class ListNode {
+    
+    private int data;
+    private ListNode next;
+
+    public ListNode(int data) {
+        this.data = data;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public ListNode getNext() {
+        return next;
+    }
+
+    public void setNext(ListNode next) {
+        this.next = next;
+    }
+    
+    
+}
