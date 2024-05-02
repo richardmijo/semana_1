fba84dca106a21af8de4545cd695325bc417e010
Richard Armijos <richardmijo@hotmail.com>
2024-05-01 20:20:25 -0500

    Creacion de persona

---
+++ src/main/java/ec/gob/loja/semana_1/Persona.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.gob.loja.semana_1;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author desarrollo
+ */
+public class Persona {
+    
+   private String identificacion;
+   private String nombre;
+   private String apellido;
+   
+   public Persona() {
+    }
+
+    public String getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+  
+}
--- src/main/java/ec/gob/loja/semana_1/Semana_1.java
+++ src/main/java/ec/gob/loja/semana_1/Semana_1.java
@@ -14,4 +14,6 @@ public class Semana_1 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+    
+    
 }
