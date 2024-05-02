51374abbc161d975f4137628f96b66c32c037615
Richard Armijos <richardmijo@hotmail.com>
2024-05-01 20:03:00 -0500

    Proyecto inicial

---
+++ src/main/java/ec/gob/loja/semana_1/Semana_1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package ec.gob.loja.semana_1;
+
+/**
+ *
+ * @author desarrollo
+ */
+public class Semana_1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
